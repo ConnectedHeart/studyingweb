8ee94969cea74bff518e2cf4fdaee2ae5a4beb5f
TaeHoon Han <othankloveo@naver.com>
2024-12-21 21:23:06 +0900

    스프링 빈 생명주기
    스프링 컨테이너 생성 -> 스프링 빈 생성 -> 의존주입 -> 초기화 콜백 -> 사용 -> 스프링 빈 소멸 콜백 -> 빈 소멸

---
+++ spring/core/src/test/java/hello/core/lifecyle/BeanLifeCycleTest.java
@@ -0,0 +1,28 @@
+package hello.core.lifecyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        // ConfigurableApplicationContext 인터페이스를 사용하거나, AnnotationConfigApplicationContext 를 사용해야 .close메소드를 쓸 수 있다.
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
