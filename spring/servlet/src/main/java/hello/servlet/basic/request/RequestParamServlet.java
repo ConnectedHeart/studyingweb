40dff974f3be26381ce6041dde337777ecf162f1
TaeHoon Han <othankloveo@naver.com>
2025-01-13 23:34:32 +0900

    HTTP POST HTML 방식, HTTP API - message body 가 text인 방식

--- spring/servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ spring/servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -30,7 +30,7 @@ public class RequestParamServlet extends HttpServlet {
 
         System.out.println("[단일 파라미터 조회]");
 
-        String username = request.getParameter("usename");
+        String username = request.getParameter("username");
         String age = request.getParameter("age");
 
         System.out.println("username = " + username);
